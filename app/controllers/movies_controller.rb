0bdc1091045741b3759a5684ac28230d0aa9b4f0
Jonathan-Miksis <jmiksis13@gmail.com>
2021-10-18 20:06:59 -0400

    add create action and route

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,5 +5,19 @@ class MoviesController < ApplicationController
     render json: movies.as_json
   end
 
+  def show
+    movie = Movie.find_by(id: params[:id])
+    render json: movie.as_json
+  end
+
+  def create
+    movie = Movie.new(
+      title: params[:title],
+      director: params[:director],
+      year: params[:year]
+    )
+    movie.save
+    render json: movie.as_json
+  end
 
 end
